62f3139bb9dc388af214378ed6ddba29b8d85771
Grégoire <72392542+Bassounet@users.noreply.github.com>
2021-02-27 12:36:06 +0100

    allé encore la cam :+1:

--- src/scenes/Tableau_test.js
+++ src/scenes/Tableau_test.js
@@ -8,10 +8,10 @@ class Tableau_test extends Tableau{
         //on définit la taille du tableau
         let largeurDuTableau=4000;
         let hauteurDuTableau=448; //la hauteur est identique au cadre du jeu
-       // this.cameras.main.setBounds(0, 0, largeurDuTableau, hauteurDuTableau);
+        this.cameras.main.setBounds(0, 0, largeurDuTableau, hauteurDuTableau);
         this.physics.world.setBounds(0, 0, largeurDuTableau,  hauteurDuTableau);
 
-        this.cameras.main.startFollow(this.player, false, 0.05, 0.05);
+        this.cameras.main.startFollow(this.player, false, 1, 1,1,1); // ici c'est beugé aïe 
 
 
     }
