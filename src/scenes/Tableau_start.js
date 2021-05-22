0ebfb3026f4ca92dd1648db3089d53227b5d3f84
Grégoire <72392542+Bassounet@users.noreply.github.com>
2021-05-22 16:48:26 +0200

    voici_le_enter_fonctionne_pour_avancer_avec_du_son

--- src/scenes/Tableau_start.js
+++ src/scenes/Tableau_start.js
@@ -3,6 +3,8 @@ class Tableau_start extends Tableau {
     preload() { 
 
         this.load.image('fond', 'assets/starting_screenV6.png');
+        this.load.audio('go', 'son/go.wav');
+
 
 
     }
@@ -13,15 +15,24 @@ class Tableau_start extends Tableau {
         super.create();
 
         this.add.image(448,224,'fond');
+        this.motiv = this.sound.add('go');
 
+        this.input.keyboard.on('keydown-ENTER', function () //'keydown-SPACE', function ()
+        {
+            if (!this.touchePressed)
+            {
+                this.touchePressed = true;
+                this.sound.play('go',{volume : 1});
+                this.cameras.main.fadeOut(1000, 0, 0, 0)
+                this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) =>
+                {
+                    Tableau.suivant();
 
-        // var timedEvent;
-        //
-        // timedEvent = this.time.addEvent({
-        //     delay: 3000,
-        //     callback: Tableau.suivant,
-        //     callbackScope: this,
-        //     loop: true });
+                })
+            }
+
+        }, this);
+
 
     }
 
